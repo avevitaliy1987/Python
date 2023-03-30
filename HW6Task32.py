b43fb1038a291110297683339954ee7728f16bfb
avevitaliy1987 <120784550+avevitaliy1987@users.noreply.github.com>
2023-03-30 22:00:47 +0500

    add HW task 30, 32

---
+++ HW6Task32.py
@@ -0,0 +1,13 @@
+# Задача 32: Определить индексы элементов массива (списка), 
+# значения которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+import random
+list_1 = [random.randint(-20, 20) for i in range(20)]
+print(list_1)
+
+min_number = int(input('введи минимальное число: '))
+max_number = int(input('введите максимальное число: '))
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
